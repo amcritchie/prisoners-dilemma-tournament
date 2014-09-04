8b5960cda4f7a2fb7394ba11a7b9a1e1cb626a8e
Alex McRitchie <amcritchie@gmail.com>
2014-09-04 08:28:42 -0600

    finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,33 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function(){
-    $('#roundOneWinners').hide();
+$(document).ready(function () {
+    $('div').css('border', 'solid blue 3px');
+
+    $('.round:nth-of-type(3)').hide();
+    $('.round:nth-of-type(5)').hide();
+    $('.round:nth-of-type(7)').hide();
+    $('.round:nth-of-type(9)').hide();
+    $('.round:nth-of-type(11)').hide();
+    $('.round:nth-of-type(13)').hide();
+    $('.round:nth-of-type(15)').hide();
+    $('.round:nth-of-type(17)').hide();
+
+
+
+    $('.results:nth-of-type(2)').hide();
+    $('.results:nth-of-type(4)').hide();
+    $('.results:nth-of-type(6)').hide();
+    $('.results:nth-of-type(8)').hide();
+    $('.results:nth-of-type(10)').hide();
+    $('.results:nth-of-type(12)').hide();
+    $('.results:nth-of-type(14)').hide();
+    $('.results:nth-of-type(16)').hide();
 
 
     $codeName = $('#codeName');
 
-    $codeName.on('keyup', function() {
+    $codeName.on('keyup', function () {
 
         $('#nameOfClass').empty().append('class ' + this.value.substr(0, 1).toUpperCase() + this.value.substr(1));
 
@@ -30,9 +50,148 @@ $(document).ready(function(){
 
     });
 
-    $("#rr1").on('click',  function(){
-        ;
-       $('#roundOneWinners').show();
+    var round = 1;
+    var rounds = 7;
+
+
+//    while (round < rounds) {
+//
+//        $("#runRound:nth-of-type(round)").on('click', function () {
+//            $('.results:nth-of-type(round + 1)').show();
+//            $('.round:nth-of-type(round)').hide();
+//        });
+//
+//        $("#nextRound:nth-of-type(round)").on('click', function () {
+//            $('.results:nth-of-type(round + 1)').hide();
+//            $('.round:nth-of-type(round + 2)').show();
+//
+//            round += 1
+//        });
+//    }
+
+    var round_action = 1;
+
+    $(".runRound").on('click', function () {
+        console.log(round_action);
+
+        if (round_action == 1){
+
+
+            $('.results:nth-of-type(2)').show();
+            $('.round:nth-of-type(1)').hide();
+            round_action = 2;
+        } else if (round_action == 2){
+
+
+            $('.results:nth-of-type(4)').show();
+            $('.round:nth-of-type(3)').hide();
+            round_action = 3;
+        } else if (round_action == 3){
+
+
+            $('.results:nth-of-type(6)').show();
+            $('.round:nth-of-type(5)').hide();
+            round_action = 4;
+        } else if (round_action == 4){
+            console.log("action 3");
+
+            $('.results:nth-of-type(8)').show();
+            $('.round:nth-of-type(7)').hide();
+
+            round_action = 5;
+        } else if (round_action == 5){
+            $('.results:nth-of-type(10)').show();
+            $('.round:nth-of-type(9)').hide();
+            round_action = 6;
+        }
+
     });
 
+    var next_action = 1;
+    $(".nextRound").on('click', function () {
+        if (next_action == 1) {
+            $('.results:nth-of-type(2)').hide();
+            $('.round:nth-of-type(3)').show();
+            next_action = 2;
+        } else if (next_action == 2) {
+
+            $('.results:nth-of-type(4)').hide();
+            $('.round:nth-of-type(5)').show();
+            next_action = 3;
+        } else if (next_action == 3) {
+
+            $('.results:nth-of-type(6)').hide();
+            $('.round:nth-of-type(7)').show();
+            next_action = 4;
+        } else if (next_action == 4) {
+
+            $('.results:nth-of-type(8)').hide();
+            $('.round:nth-of-type(9)').show();
+            next_action = 5;
+        } else if (next_action == 5) {
+
+            $('.results:nth-of-type(10)').hide();
+            $('.round:nth-of-type(11)').show();
+            next_action = 6;
+        }
+        else if (next_action == 6) {
+
+            $('.results:nth-of-type(12)').hide();
+            $('.round:nth-of-type(13)').show();
+            next_action = 7;
+        }
+    });
+
+//    $("#runRound:nth-of-type(1)").on('click', function () {
+//        $('div').css('border', 'solid red 3px');
+//
+//        $('.results:nth-of-type(2)').show();
+//        $('.round:nth-of-type(1)').hide();
+//    });
+//
+//    $("#nextRound:nth-of-type(1)").on('click', function () {
+//        $('div').css('border', 'solid orange 3px');
+//
+//        $('.results:nth-of-type(2)').hide();
+//        $('.round:nth-of-type(3)').show();
+//    });
+
+//
+//    $("#runRound:nth-of-type(2)").on('click',  function(){
+//        $('div').css('border', 'solid yellow 3px');
+//
+//        $('.results:nth-of-type(4)').show();
+//        $('.round:nth-of-type(3)').hide();
+//        $('.round:nth-of-type(1)').hide();
+//
+//    });
+//
+//    $("#nextRound:nth-of-type(2)").on('click',  function(){
+//        $('div').css('border', 'solid green 3px');
+//
+//        $('.results:nth-of-type(4)').hide();
+//        $('.round:nth-of-type(5)').show();
+//    });
+//
+//
+//    $("#runRound:nth-of-type(3)").on('click',  function(){
+//        $('.results:nth-of-type(2)').show();
+//        $('.round:nth-of-type(1)').hide();
+//    });
+//
+//    $("#nextRound:nth-of-type(3)").on('click',  function(){
+//        $('.results:nth-of-type(2)').hide();
+//        $('.round:nth-of-type(3)').show();
+//    });
+//
+//
+//    $("#runRound:nth-of-type(4)").on('click',  function(){
+//        $('.results:nth-of-type(2)').show();
+//        $('.round:nth-of-type(1)').hide();
+//    });
+//
+//    $("#nextRound:nth-of-type(4)").on('click',  function(){
+//        $('.results:nth-of-type(2)').hide();
+//        $('.round:nth-of-type(3)').show();
+//    });
 });
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -17,27 +17,34 @@ class TournamentsController < ApplicationController
 
 
     # Scoring!
-    cooperate_score = 100
-    defect_score = -100
-    win_score = 500
-    lose_score = -600
-
-    @winners = []
-
+    cooperate_score = 3
+    defect_score = -4
+    win_score = 7
+    lose_score = -1
     @users = User.all
     users = []
+
+    @round_players = []
+    @round_winners = []
+
     count = @users.length
+
 
     @users.each do |user|
       users.push(user)
     end
 
+    count = users.length
 
-    count = @users.length
-
+    while count > 1
 
     @challenger_array = users.shuffle
 
+    @round_players.push(@challenger_array.dup)
+
+    @results = []
+    @winners = []
+    odd = false
 
     challenger = 0
 
@@ -46,8 +53,9 @@ class TournamentsController < ApplicationController
       challenger1 = @challenger_array[challenger]
 
       if (1 != count) && (count.even?)
-      challenger2 = @challenger_array[challenger + 1]
+        challenger2 = @challenger_array[challenger + 1]
       elsif (1 == count) && (count.odd?)
+        odd = true
         challenger2 = @challenger_array[challenger]
       else
         challenger2 = @challenger_array[challenger + 1]
@@ -63,6 +71,11 @@ class TournamentsController < ApplicationController
 
         p1choice = p1.choice(challenger1_record, cooperate_score, defect_score, win_score, lose_score)
         p2choice = p2.choice(challenger2_record, cooperate_score, defect_score, win_score, lose_score)
+
+
+        # Screw you hackers!
+        !!p1choice
+        !!p2choice
 
         if p1choice
 
@@ -106,18 +119,34 @@ class TournamentsController < ApplicationController
       player2_final = challenger2_record.inject { |sum, x| sum + x }
 
       if player1_final >= player2_final
-        @winners.push(challenger1.name)
+        @winners.push(challenger1)
       else
-        @winners.push(challenger2.name)
+        @winners.push(challenger2)
+      end
+
+      @results.push([challenger1.name, player1_final])
+      if count != 1
+        @results.push([challenger2.name, player2_final])
       end
 
       @winners
     end
 
-    # GET /users/1/edit
-    def edit
+    @round_winners.push(@results.dup)
+    p "-"*80
+    p @winners
+    p "="*80
+
+    users = @winners
+    count = users.length
     end
 
+    @round_winners.push(["Winner!"])
+    @round_players.push(@winners.dup)
 
+  end
+
+  # GET /users/1/edit
+  def edit
   end
 end
